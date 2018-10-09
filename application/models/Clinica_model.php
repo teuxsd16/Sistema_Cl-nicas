8a97541fdc7491747dfe8108566689ff012177a7
Matheus Carvalho <matheus_carvalho192@outlook.com>
2018-10-09 20:51:44 -0300

    Update application/models/Clinica_model.php

--- application/models/Clinica_model.php
+++ application/models/Clinica_model.php
@@ -4,22 +4,5 @@ class Clinica_model extends MY_Model {
         parent::__construct();
         $this->table = '';
     }
-    /**
-    * Formata os contatos para exibição dos dados na home
-    *
-    * @param array $contatos Lista dos contatos a serem formatados
-    *
-    * @return array
-    */
-    function Formatar($contatos){
-      if($contatos){
-        for($i = 0; $i < count($contatos); $i++){
-          $contatos[$i]['editar_url'] = base_url('editar')."/".$contatos[$i]['id'];
-          $contatos[$i]['excluir_url'] = base_url('excluir')."/".$contatos[$i]['id'];
-        }
-        return $contatos;
-      } else {
-        return false;
-      }
-    }
+    
 }
