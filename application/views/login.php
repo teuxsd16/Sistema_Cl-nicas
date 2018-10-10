8ef68479d6b675939e64632473e6777a5beefd8b
Matheus Carvalho <matheus_carvalho192@outlook.com>
2018-10-10 15:06:11 -0300

    kk

--- application/views/login.php
+++ application/views/login.php
@@ -4,6 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <title>Painel de Acesso</title>
+  <!-- Bootstrap core CSS -->
+    <link href=<?= base_url("vendor/bootstrap/css/bootstrap.min.css") ?> rel="stylesheet">  
   <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 
@@ -57,27 +59,29 @@
 
           <form action="/" method="post">
 
-          <div class="top-row">
-            <div class="field-wrap">
-              <label>
-                Nome<span class="req">*</span>
-              </label>
-              <input type="text" required autocomplete="off" />
-            </div>
-
-            <div class="field-wrap">
-              <label>
-                Sobrenome<span class="req">*</span>
-              </label>
-              <input type="text"required autocomplete="off"/>
-            </div>
+          
+          <div class="field-wrap">
+            <label>
+              Nome<span class="req">*</span>
+            </label>
+            <input type="text" required autocomplete="off" />
+  
           </div>
+
+        
 
           <div class="field-wrap">
             <label>
               Email<span class="req">*</span>
             </label>
             <input type="email"required autocomplete="off"/>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Telefone<span class="req">*</span>
+            </label>
+            <input type="password"required autocomplete="off"/>
           </div>
 
           <div class="field-wrap">
@@ -94,6 +98,48 @@
               <option>Clínica</option>
             </select>
           </div>
+
+          <div class="invisible">
+            <!-- Médico -->
+            <div class="field-wrap">
+              <label>
+                CRN<span class="req">*</span>
+              </label>
+              <input name="crn" type="text"required autocomplete="off"/>
+            </div>
+          </div>  
+          <div class="invisible">
+            <!-- Clínica/posto -->
+            <div class="field-wrap">
+              <label>
+                CNPJ<span class="req">*</span>
+              </label>
+              <input name="cnpj" type="text"required autocomplete="off"/>
+            </div>
+          </div>
+
+          <!-- Endereço -->
+          <div class="field-wrap">
+            <label>
+              Bairro<span class="req">*</span>
+            </label>
+            <input name="bairro" type="text"required autocomplete="off"/>
+          </div>
+          <div class="field-wrap">
+            <label>
+              Rua<span class="req">*</span>
+            </label>
+            <input name="rua" type="text"required autocomplete="off"/>
+          </div>
+          <div class="field-wrap">
+            <label>
+              Número<span class="req">*</span>
+            </label>
+            <input name="numero" type="text"required autocomplete="off"/>
+          </div>
+
+
+
 
           
           <button type="submit" class="button button-block"/>Cadastrar</button>
