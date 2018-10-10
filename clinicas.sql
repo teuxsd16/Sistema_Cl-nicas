1725341986d8c1e012323e54c86743f7d53fd125
DjairMaykon <djairmaykon@gmail.com>
2018-10-10 17:16:12 -0300

    Update Banco

--- clinicas.sql
+++ clinicas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 08-Out-2018 às 22:10
--- Versão do servidor: 10.1.32-MariaDB
--- PHP Version: 5.6.36
+-- Host: localhost
+-- Generation Time: 10-Out-2018 às 22:13
+-- Versão do servidor: 10.1.35-MariaDB
+-- versão do PHP: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,12 +25,14 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `avalia`
+-- Estrutura da tabela `avalia_servico`
 --
 
-CREATE TABLE `avalia` (
-  `Cod_Cliente` int(11) NOT NULL,
-  `Cod_Clinica` int(11) NOT NULL
+CREATE TABLE `avalia_servico` (
+  `cliente_cliente_id` int(11) NOT NULL,
+  `fornece_fornece_id` int(11) NOT NULL,
+  `comentario` varchar(255) DEFAULT NULL,
+  `nota` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -40,10 +42,11 @@ CREATE TABLE `avalia` (
 --
 
 CREATE TABLE `cliente` (
-  `Nome_cliente` varchar(255) DEFAULT NULL,
-  `Email` varchar(255) DEFAULT NULL,
-  `Senha` varchar(255) DEFAULT NULL,
-  `Cod_Cliente` int(11) NOT NULL
+  `cliente_id` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `telefone` varchar(255) DEFAULT NULL,
+  `endereco_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -53,21 +56,54 @@ CREATE TABLE `cliente` (
 --
 
 CREATE TABLE `clinica` (
-  `Nome_clinica` varchar(255) DEFAULT NULL,
-  `Cod_Clinica` int(11) NOT NULL,
-  `Telefone` varchar(255) DEFAULT NULL,
-  `Localizacao` varchar(255) DEFAULT NULL
+  `clinica_id` int(11) NOT NULL,
+  `razao` varchar(255) NOT NULL,
+  `cnpj` varchar(14) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `telefone` varchar(45) DEFAULT NULL,
+  `endereco_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `possui`
+-- Estrutura da tabela `endereco`
 --
 
-CREATE TABLE `possui` (
-  `Cod_Serv` int(11) NOT NULL,
-  `Cod_Clinica` int(11) NOT NULL
+CREATE TABLE `endereco` (
+  `endereco_id` int(11) NOT NULL,
+  `cep` int(11) DEFAULT NULL,
+  `bairro` varchar(45) NOT NULL,
+  `rua` varchar(45) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `referencia` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `fornece`
+--
+
+CREATE TABLE `fornece` (
+  `fornece_id` int(11) NOT NULL,
+  `clinica_clinica_id` int(11) DEFAULT NULL,
+  `servico_servico_id` int(11) NOT NULL,
+  `medico_medico_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `medico`
+--
+
+CREATE TABLE `medico` (
+  `medico_id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `telefone` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `endereco_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -77,8 +113,9 @@ CREATE TABLE `possui` (
 --
 
 CREATE TABLE `servico` (
-  `Nome_serv` varchar(255) DEFAULT NULL,
-  `Cod_Serv` int(11) NOT NULL
+  `servico_id` int(11) NOT NULL,
+  `descricao` varchar(100) NOT NULL,
+  `valor` decimal(10,5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -86,54 +123,125 @@ CREATE TABLE `servico` (
 --
 
 --
--- Indexes for table `avalia`
+-- Indexes for table `avalia_servico`
 --
-ALTER TABLE `avalia`
-  ADD PRIMARY KEY (`Cod_Cliente`,`Cod_Clinica`),
-  ADD KEY `Cod_Clinica` (`Cod_Clinica`);
+ALTER TABLE `avalia_servico`
+  ADD PRIMARY KEY (`cliente_cliente_id`,`fornece_fornece_id`),
+  ADD KEY `fk_cliente_has_fornece_fornece1_idx` (`fornece_fornece_id`),
+  ADD KEY `fk_cliente_has_fornece_cliente1_idx` (`cliente_cliente_id`);
 
 --
 -- Indexes for table `cliente`
 --
 ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`Cod_Cliente`);
+  ADD PRIMARY KEY (`cliente_id`),
+  ADD KEY `fk_cliente_endereco1_idx` (`endereco_id`);
 
 --
 -- Indexes for table `clinica`
 --
 ALTER TABLE `clinica`
-  ADD PRIMARY KEY (`Cod_Clinica`);
+  ADD PRIMARY KEY (`clinica_id`),
+  ADD KEY `fk_clinica_endereco1_idx` (`endereco_id`);
 
 --
--- Indexes for table `possui`
+-- Indexes for table `endereco`
 --
-ALTER TABLE `possui`
-  ADD PRIMARY KEY (`Cod_Serv`,`Cod_Clinica`),
-  ADD KEY `Cod_Clinica` (`Cod_Clinica`);
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`endereco_id`);
+
+--
+-- Indexes for table `fornece`
+--
+ALTER TABLE `fornece`
+  ADD PRIMARY KEY (`fornece_id`),
+  ADD KEY `fk_clinica_has_servico_servico1_idx` (`servico_servico_id`),
+  ADD KEY `fk_clinica_has_servico_clinica1_idx` (`clinica_clinica_id`),
+  ADD KEY `fk_fornece_medico1_idx` (`medico_medico_id`);
+
+--
+-- Indexes for table `medico`
+--
+ALTER TABLE `medico`
+  ADD PRIMARY KEY (`medico_id`),
+  ADD KEY `fk_medico_endereco1_idx` (`endereco_id`);
 
 --
 -- Indexes for table `servico`
 --
 ALTER TABLE `servico`
-  ADD PRIMARY KEY (`Cod_Serv`);
+  ADD PRIMARY KEY (`servico_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `clinica`
+--
+ALTER TABLE `clinica`
+  MODIFY `clinica_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `endereco`
+--
+ALTER TABLE `endereco`
+  MODIFY `endereco_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `medico`
+--
+ALTER TABLE `medico`
+  MODIFY `medico_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `servico`
+--
+ALTER TABLE `servico`
+  MODIFY `servico_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Limitadores para a tabela `avalia`
+-- Limitadores para a tabela `avalia_servico`
 --
-ALTER TABLE `avalia`
-  ADD CONSTRAINT `avalia_ibfk_1` FOREIGN KEY (`Cod_Clinica`) REFERENCES `clinica` (`Cod_Clinica`),
-  ADD CONSTRAINT `avalia_ibfk_2` FOREIGN KEY (`Cod_Cliente`) REFERENCES `cliente` (`Cod_Cliente`);
+ALTER TABLE `avalia_servico`
+  ADD CONSTRAINT `fk_cliente_has_fornece_cliente1` FOREIGN KEY (`cliente_cliente_id`) REFERENCES `cliente` (`cliente_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_cliente_has_fornece_fornece1` FOREIGN KEY (`fornece_fornece_id`) REFERENCES `fornece` (`fornece_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Limitadores para a tabela `possui`
+-- Limitadores para a tabela `cliente`
 --
-ALTER TABLE `possui`
-  ADD CONSTRAINT `possui_ibfk_1` FOREIGN KEY (`Cod_Serv`) REFERENCES `servico` (`Cod_Serv`),
-  ADD CONSTRAINT `possui_ibfk_2` FOREIGN KEY (`Cod_Clinica`) REFERENCES `clinica` (`Cod_Clinica`);
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `fk_cliente_endereco1` FOREIGN KEY (`endereco_id`) REFERENCES `endereco` (`endereco_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `clinica`
+--
+ALTER TABLE `clinica`
+  ADD CONSTRAINT `fk_clinica_endereco1` FOREIGN KEY (`endereco_id`) REFERENCES `endereco` (`endereco_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `fornece`
+--
+ALTER TABLE `fornece`
+  ADD CONSTRAINT `fk_clinica_has_servico_clinica1` FOREIGN KEY (`clinica_clinica_id`) REFERENCES `clinica` (`clinica_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_clinica_has_servico_servico1` FOREIGN KEY (`servico_servico_id`) REFERENCES `servico` (`servico_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_fornece_medico1` FOREIGN KEY (`medico_medico_id`) REFERENCES `medico` (`medico_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `medico`
+--
+ALTER TABLE `medico`
+  ADD CONSTRAINT `fk_medico_endereco1` FOREIGN KEY (`endereco_id`) REFERENCES `endereco` (`endereco_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
